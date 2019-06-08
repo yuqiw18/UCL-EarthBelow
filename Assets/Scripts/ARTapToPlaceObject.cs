53ffed030a664ed96e864a6c5e0ddbf2306aa56f
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2019-06-08 15:02:23 +0100

    Update to AR Foundation

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private ARRaycastManager arRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+
+            PlaceObject();
+        }
+
+    }
+
+    private void UpdatePlacementPose()
+    {
+        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            Vector3 cameraForward = Camera.current.transform.forward;
+            Vector3 cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator() { 
+   
+        if (placementPoseIsValid) {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void PlaceObject(){
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+
+}
--- README.md
+++
@@ -1,7 +0,0 @@
-# EarthBelow iOS Port #
-
-## Requirements: ##
-* [Unity](https://unity3d.com/get-unity/download) v2017.4+
-* Apple [Xcode](https://developer.apple.com/xcode/) 10.0+ with latest iOS SDK that contains ARKit Framework
-* Apple iOS device that supports ARKit (iPhone 6S or later, iPad (2017) or later)
-* Apple [iOS 12](https://www.apple.com/ios/ios-12/)+ installed on device
