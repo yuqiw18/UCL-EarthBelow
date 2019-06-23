05cc03ad9dd2d670d569b66d30f93b410efde08b
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2019-06-23 20:06:27 +0100

    Material Update

---
+++ Assets/Scripts/SpawnVFX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnVFX : MonoBehaviour
+{
+    private float currentValue = 1;
+    private float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentValue > 0) {
+            currentValue -= speed * Time.deltaTime;
+            if (currentValue < 0)
+            {
+                currentValue = 0;
+                GetComponent<Renderer>().sharedMaterial.SetFloat("_Dissolve", currentValue);
+            }
+            else {
+                GetComponent<Renderer>().sharedMaterial.SetFloat("_Dissolve", currentValue);
+            }
+
+        }
+    }
+}
