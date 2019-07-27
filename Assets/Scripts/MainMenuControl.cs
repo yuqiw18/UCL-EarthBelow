d78db7b3c0f376473d3acece02ee256ce3fea869
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2019-07-27 19:06:38 +0100

    Main menu update

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -6,43 +6,61 @@ public class MainMenuControl : MonoBehaviour
 {
     private GameObject menuBackground;
 
-    public enum SHIFTMODE {TOP, BOTTOM, LEFT, RIGHT, CENTER };
 
+    private Vector3 originPostion;
     private Vector3 targetPosition;
+    
+    private float speed = 100.0f;
+
+    private bool canMove = false;
     //private float targetTransf;
 
     // Start is called before the first frame update
     void Start()
     {
         menuBackground = this.transform.Find("Image_Background").gameObject;
-        Debug.Log("Found");
+        originPostion = menuBackground.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if (canMove){
+            float step =  speed * Time.deltaTime;
+            menuBackground.transform.position = Vector3.MoveTowards(menuBackground.transform.position, targetPosition, step);
+            Vector3 tempPosition = menuBackground.transform.position;
+            if (tempPosition == targetPosition){
+                canMove = false;
+                Debug.Log("Reached");
+            }
+         }
     }
 
-    public void MoveBackground(SHIFTMODE operation) {
+    public void MoveBackground(string operation) {
 
         switch (operation) {
-
-            case SHIFTMODE.TOP:
-                targetPosition = new Vector3(0, Screen.height / 2, 0);
+            case "TOP":
+                targetPosition = originPostion + new Vector3(0, Screen.height / 2, 0);
                 break;
-            case SHIFTMODE.BOTTOM:
-                targetPosition = new Vector3(0, -Screen.height / 2, 0);
+            case "BOTTOM":
+                targetPosition = originPostion + new Vector3(0, -Screen.height / 2, 0);
                 break;
-            case SHIFTMODE.LEFT:
-                targetPosition = new Vector3(0, -Screen.width / 2, 0);
+            case "LEFT":
+                targetPosition = originPostion + new Vector3(-Screen.width / 2, 0, 0);
                 break;
-            case SHIFTMODE.RIGHT:
-                targetPosition = new Vector3(0, Screen.width / 2, 0);
+            case "RIGHT":
+                targetPosition = originPostion + new Vector3(Screen.width / 2, 0, 0);
                 break;
-            case SHIFTMODE.CENTER:
-                targetPosition = new Vector3(0, 0, 0);
+            case "CENTER":
+                targetPosition = originPostion;
+                break;
+            default: 
                 break;
         }
+        canMove = true;
+        speed = Vector3.Distance(menuBackground.transform.position, targetPosition);
+
+        Debug.Log("New Speed:" + speed);
+        Debug.Log("Triggered");
     }
 }
---
+++ Assets/Scripts/UIFadeInOut.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFadeInOut : MonoBehaviour
+{
+    private Text text;
+    // Start is called before the first frame update
+
+    void Awake(){
+        text = this.GetComponent<Text>();
+    }
+
+    private void OnEnable(){
+        FadeIn();
+    }
+
+    public void FadeIn(){
+        StartCoroutine(FadeToFullAlpha());
+    }
+
+    public void FadeOut(){
+        StartCoroutine(FadeToZeroAlpha());
+    }
+
+    private IEnumerator FadeToFullAlpha()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeToZeroAlpha()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - Time.deltaTime);
+            yield return null;
+        }
+    }
+
+}
